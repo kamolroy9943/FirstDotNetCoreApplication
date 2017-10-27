bb166b161da4cc1a938313c9f1323b70b334a57f
Kamol Roy <kamol.roy9943@gmail.com>
2017-10-27 17:40:56 +0600

    Rendering with razor view with a controller

--- FirstDotNetCoreApp/src/FirstDotNetCoreApp/Controllers/HomeController.cs
+++ FirstDotNetCoreApp/src/FirstDotNetCoreApp/Controllers/HomeController.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FirstDotNetCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FirstDotNetCoreApp.Controllers
 {
-    public class HomeController
+    public class HomeController:Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello i am from Home controller";
+            var restaurent = new Restaurent() {Id =1, Name = "Pizaa Hat"};
+            return View(restaurent);
         }
     }
 }
